fdc3fd58c61ee617472a3ea597fdf07f45e471c3
VishwasMallikarjuna <vishwasdm596@gmail.com>
2024-05-06 11:51:19 +0200

    updating Echo server

--- src/main.go
+++ src/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "github.com/labstack/echo"
+import (
+	"os"
+
+	"github.com/labstack/echo"
+)
 
 func main() {
 	e := echo.New()
+	retCode, startServer, _ := configureMgmtServer(e, os.Args[1:])
+	if retCode != 0 {
+		os.Exit(retCode)
+	}
+
+	startServer()
+	os.Exit(0)
 }
