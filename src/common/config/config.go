438c9978c5749259d6ae6b9cc0753aa8f46bf2dd
VishwasMallikarjuna <vishwasdm596@gmail.com>
2024-05-13 01:06:40 +0200

    updated config implementation

--- src/common/config/config.go
+++ src/common/config/config.go
@@ -2,7 +2,12 @@ package config
 
 import "flag"
 
+type Config struct {
+}
+
 func GetConfig(configPath string, commandLineFlags []string) (Config, error) {
 	fs := flag.NewFlagSet("goMongoConfig", flag.ContinueOnError)
-	
+
+	config := Config{}
+
 }
