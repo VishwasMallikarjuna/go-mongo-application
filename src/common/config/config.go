f060a523949d6982bf5d220166846e046cc66c0d
VishwasMallikarjuna <vishwasdm596@gmail.com>
2024-05-14 19:00:42 +0200

    updated config struct

--- src/common/config/config.go
+++ src/common/config/config.go
@@ -3,6 +3,11 @@ package config
 import "flag"
 
 type Config struct {
+	ConfigPath         string
+	LogLevel           string
+	MongoDBUri         string
+	MongoDBName        string
+	MongoColName       string
 }
 
 func GetConfig(configPath string, commandLineFlags []string) (Config, error) {
