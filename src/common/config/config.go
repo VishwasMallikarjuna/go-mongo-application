fb1bf2629f97044ad86e0081e7abdeacae7f192c
VishwasMallikarjuna <vishwasdm596@gmail.com>
2024-05-15 16:30:03 +0200

    updated GetConfig

--- src/common/config/config.go
+++ src/common/config/config.go
@@ -3,16 +3,18 @@ package config
 import "flag"
 
 type Config struct {
-	ConfigPath         string
-	LogLevel           string
-	MongoDBUri         string
-	MongoDBName        string
-	MongoColName       string
+	ConfigPath   string
+	LogLevel     string
+	MongoDBUri   string
+	MongoDBName  string
+	MongoColName string
 }
 
 func GetConfig(configPath string, commandLineFlags []string) (Config, error) {
 	fs := flag.NewFlagSet("goMongoConfig", flag.ContinueOnError)
 
 	config := Config{}
+	fs.StringVar(&config.ConfigPath, "config-path", configPath, "(Optional) Path of an alternate config file")
 
+	return config, nil
 }
