8808d8c02432d2220dc3cc4e6c8521b9be03ed3e
VishwasMallikarjuna <vishwasdm596@gmail.com>
2024-06-07 13:16:28 +0200

    updated package logwrapper

--- src/common/logwrapper/wrapper.go
+++ src/common/logwrapper/wrapper.go
@@ -12,3 +12,16 @@ type LogConfig struct {
 }
 
 var globalConfig LogConfig
+
+func Initialize(lvlStr string, out io.Writer) (*LogConfig, error) {
+	parsedLvl, err := ParseLevelFromStr(lvlStr)
+	if err != nil {
+		return nil, err
+	}
+
+	globalConfig = LogConfig{
+		Level:    parsedLvl,
+		Location: out,
+	}
+	return &globalConfig, nil
+}
