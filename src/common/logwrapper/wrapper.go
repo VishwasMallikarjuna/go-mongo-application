06892379aabdf8ab546243ecf3a7a18c93bca62c
VishwasMallikarjuna <vishwasdm596@gmail.com>
2024-06-08 00:06:21 +0200

    updated package logwrapper

--- src/common/logwrapper/wrapper.go
+++ src/common/logwrapper/wrapper.go
@@ -1,6 +1,7 @@
 package logwrapper
 
 import (
+	"fmt"
 	"io"
 
 	"github.com/sirupsen/logrus"
@@ -24,4 +25,13 @@ func Initialize(lvlStr string, out io.Writer) (*LogConfig, error) {
 		Location: out,
 	}
 	return &globalConfig, nil
+}
+
+func ParseLevelFromStr(lvlStr string) (logrus.Level, error) {
+	level, err := logrus.ParseLevel(lvlStr)
+	if err != nil {
+		wrappedErr := fmt.Errorf("error parsing log Level - %w", err)
+		return logrus.PanicLevel, wrappedErr ///in logrus, Default Level => Panic
+	}
+	return level, nil
 }
