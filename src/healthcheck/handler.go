64c1c1eff6aee381af796a8f86e8abf0b3a51d18
VishwasMallikarjuna <vishwasdm596@gmail.com>
2024-07-10 10:30:46 +0200

    Updated healthCheck handler

--- src/healthcheck/handler.go
+++ src/healthcheck/handler.go
@@ -1,8 +1,15 @@
 package healthcheck
 
+import "go.mongodb.org/mongo-driver/mongo"
+
 func NewHandler(config configPkg.Config) Handler {
 	return &theHandler{
 		config:         config,
 		hriHealthcheck: GetCheck,
 	}
+}
+
+type theHandler struct {
+	config         configPkg.Config
+	hriHealthcheck func(string, *mongo.Collection) (int, *response.ErrorDetail)
 }
