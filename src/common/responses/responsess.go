a831748e084fa409ec8dd0879b4fd9084b4150c0
VishwasMallikarjuna <vishwasdm596@gmail.com>
2024-07-17 17:40:48 +0200

    Updated response

--- src/common/responses/responsess.go
+++ src/common/responses/responsess.go
@@ -4,3 +4,11 @@ type ErrorDetail struct {
 	ErrorEventId     string `json:"errorEventId"`
 	ErrorDescription string `json:"errorDescription"`
 }
+
+func NewErrorDetail(requestId string, description string) *ErrorDetail {
+	errorDetail := ErrorDetail{
+		ErrorEventId:     requestId,
+		ErrorDescription: description,
+	}
+	return &errorDetail
+}
