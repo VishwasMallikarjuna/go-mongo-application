27b427edb709cb397792f046456b83e6fd68c05e
VishwasMallikarjuna <vishwasdm596@gmail.com>
2024-07-23 14:26:52 +0200

    Updated healthCheck Handler

--- src/healthcheck/handler.go
+++ src/healthcheck/handler.go
@@ -2,13 +2,14 @@ package healthcheck
 
 import (
 	configPkg "github.com/VishwasMallikarjuna/go-mongo-appliacation/common/config"
+	"github.com/VishwasMallikarjuna/go-mongo-appliacation/common/logwrapper"
 	"github.com/VishwasMallikarjuna/go-mongo-appliacation/common/response"
 	"github.com/labstack/echo"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type theHandler struct {
-	config         configPkg.Config
+	config      configPkg.Config
 	healthcheck func(string, *mongo.Collection) (int, *response.ErrorDetail)
 }
 
@@ -24,6 +25,11 @@ func NewHandler(config configPkg.Config) Handler {
 }
 
 func (h *theHandler) Healthcheck(c echo.Context) error {
-	
+
+	requestId := c.Response().Header().Get(echo.HeaderXRequestID)
+	prefix := "healthcheck/handler"
+	var logger = logwrapper.GetMyLogger(requestId, prefix)
+	logger.Debug("Start Healthcheck Handler")
+
 	return c.NoContent(code)
 }
