6d2fe51f44c0dd46e22858373cd3cadde3f591da
VishwasMallikarjuna <vishwasdm596@gmail.com>
2024-07-28 11:40:27 +0200

    updated get_check

--- src/healthcheck/get_check.go
+++ src/healthcheck/get_check.go
@@ -1,10 +1,13 @@
 package healthcheck
 
 import (
+	"github.com/VishwasMallikarjuna/go-mongo-appliacation/common/logwrapper"
 	"github.com/VishwasMallikarjuna/go-mongo-appliacation/common/response"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func GetCheck(requestId string, client *mongo.Collection) (int, *response.ErrorDetail) {
-
+	prefix := "healthcheck/getCheck"
+	var logger = logwrapper.GetMyLogger(requestId, prefix)
+	logger.Infof("Prepare HealthCheck - mongDb (No Input Params)")
 }
