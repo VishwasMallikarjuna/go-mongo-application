10d9d5cc32a9cbf2dd352dbe229fc09065b82cbf
VishwasMallikarjuna <vishwasdm596@gmail.com>
2024-07-30 12:23:17 +0200

    updated mongoApi

--- src/healthcheck/get_check.go
+++ src/healthcheck/get_check.go
@@ -1,6 +1,8 @@
 package healthcheck
 
 import (
+	"net/http"
+
 	"github.com/VishwasMallikarjuna/go-mongo-appliacation/common/logwrapper"
 	"github.com/VishwasMallikarjuna/go-mongo-appliacation/common/response"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -10,4 +12,10 @@ func GetCheck(requestId string, client *mongo.Collection) (int, *response.ErrorD
 	prefix := "healthcheck/getCheck"
 	var logger = logwrapper.GetMyLogger(requestId, prefix)
 	logger.Infof("Prepare HealthCheck - mongDb (No Input Params)")
+
+	health_status, _, err := mongoApi.DatabaseHealthCheck(client)
+
+	if err != nil {
+		return http.StatusServiceUnavailable, mongoApi.LogAndBuildErrorDetail(requestId, http.StatusServiceUnavailable, logger, "Could not perform Cosmos health check")
+	}
 }
--- src/mongoApi/client.go
+++ src/mongoApi/client.go
@@ -1,7 +1,21 @@
 package mongoApi
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 func GetMongoCollection(collectionName string) *mongo.Collection {
 	return db.Collection(collectionName)
+}
+
+func DatabaseHealthCheck(client *mongo.Collection) (string, string) {
+	command := bson.D{{"dbStats", 1}}
+	var result bson.D
+	client.Database().RunCommand(context.TODO(), command).Decode(&result)
+	fmt.Println(result)
+	return fmt.Sprint(result[6].Value), fmt.Sprint(result[4].Value)
 }
