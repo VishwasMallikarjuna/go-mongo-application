0052fb728773fd9a9e082f33cc788709843d93a2
VishwasMallikarjuna <vishwasdm596@gmail.com>
2024-08-02 22:50:39 +0200

    updated mongo HealthCheck

--- src/mongoApi/client.go
+++ src/mongoApi/client.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/VishwasMallikarjuna/go-mongo-appliacation/common/response"
+	"github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -18,4 +20,11 @@ func DatabaseHealthCheck(client *mongo.Collection) (string, string, error) {
 	err := client.Database().RunCommand(context.TODO(), command).Decode(&result)
 	// fmt.Println(result)
 	return fmt.Sprint(result[6].Value), fmt.Sprint(result[4].Value), err
+}
+
+func LogAndBuildErrorDetail(requestId string, code int, logger logrus.FieldLogger,
+	message string) *response.ErrorDetail {
+	err := fmt.Errorf("%s: [%d]", message, code)
+	logger.Errorln(err.Error())
+	return response.NewErrorDetail(requestId, err.Error())
 }
