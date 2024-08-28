adce4354c86899af2d7e9894de01cd0d760477bf
VishwasMallikarjuna <vishwasdm596@gmail.com>
2024-08-28 15:06:55 +0200

    updated handler

--- src/mongo/handler.go
+++ src/mongo/handler.go
@@ -1,1 +1,9 @@
 package mongo
+
+import "github.com/VishwasMallikarjuna/go-mongo-application/common/config"
+
+func NewHandler(config config.Config) Handler {
+	return &theHandler{
+		config: config,
+	}
+}
