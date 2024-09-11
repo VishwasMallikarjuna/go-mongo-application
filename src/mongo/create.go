34b25b6ed8067c1a64eaa3a2988e278fe461e43a
VishwasMallikarjuna <vishwasdm596@gmail.com>
2024-09-11 15:26:23 +0530

    initiated create test

--- src/mongo/create.go
+++ src/mongo/create.go
@@ -2,8 +2,6 @@ package mongo
 
 import "net/http"
 
-func Create(
-	requestId string,
-	createId string) (int, interface{}) {
+func Create(requestId string, createId string) (int, interface{}) {
 	return http.StatusCreated, nil
 }
